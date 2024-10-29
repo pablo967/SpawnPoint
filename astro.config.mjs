a5189ec6845ce4ae7c086fdfd022bcaafafea15b
Natalia-Ramirez-Benito <natalia.ramirez.benito@gmail.com>
2024-10-29 14:28:45 +0100

    Login and Navbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,11 @@ import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
 
+import pageInsight from 'astro-page-insight';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({
     applyBaseStyles: false,
-  }), react()],
+  }), react(), pageInsight()],
 });
